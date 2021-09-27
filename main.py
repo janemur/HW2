a11ada57617718b414915c3cf7659a6342816f8a
e.a.murzaeva <murzaeva-1999@mail.ru>
2021-09-27 23:09:09 +0300

    aaa

---
+++ main.py
@@ -0,0 +1,105 @@
+import csv
+
+
+# Функция 1
+def option1(count, d):
+    """Функция принимает пустой словарь и счетчик, затем считаывает все строки из файла и выводит словарь с
+    департаментами и отделами в нем """
+    with open("Corp Summary.csv", encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=";")
+        # Функция 1
+        for row in file_reader:
+            if count == 0:
+                pass
+            else:
+                if row[1] not in d:
+                    d[row[1]] = list()
+                    d[row[1]].append(row[2])
+                else:
+                    if row[2] in [x for v in d.values() for x in v]:
+                        pass
+                    else:
+                        d[row[1]].append(row[2])
+            count += 1
+        print(d)
+
+
+# Функция 2
+def option2(count, workers_num, s):
+    """ Функция принимает пустой словарь и два счетчика, затем считывает все строки из файла и выводит словарь с
+    каждым из департаментов и статистику по нему """
+    with open("Corp Summary.csv", encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=";")
+        # Функция 1
+        for row in file_reader:
+            if count == 0:
+                pass
+            else:
+                if row[1] not in s:
+                    s[row[1]] = list()
+                    s[row[1]].append(workers_num + 1)
+                    s[row[1]].append(row[5])
+                    s[row[1]].append(row[5])
+                    s[row[1]].append(int(row[5]))
+                    s[row[1]].append(0)
+                else:
+                    (s.get(row[1]))[0] += 1
+                    (s.get(row[1]))[3] += int(row[5])
+                    (s.get(row[1]))[4] = round((s.get(row[1]))[3] / (s.get(row[1]))[0])
+                    if int(row[5]) > int((s.get(row[1]))[1]):
+                        (s.get(row[1]))[1] = int(row[5])
+                    if int(row[5]) < int((s.get(row[1]))[2]):
+                        (s.get(row[1]))[2] = int(row[5])
+            count += 1
+
+        for v in s.values():
+            del (v[3])
+
+    print(s)
+
+
+# Функция 3
+def option3(count, workers_num, k):
+    """Функция принимает пустой словарь и счетчик, затем считаывает все строки из файла, собирает словарь со
+    статистикой по отделам и записывает ее в csv файл """
+    with open("Corp Summary.csv", encoding='utf-8') as r_file:
+        file_reader = csv.reader(r_file, delimiter=";")
+        for row in file_reader:
+            if count == 0:
+                pass
+            else:
+                if row[1] not in k:
+                    k[row[1]] = list()
+                    k[row[1]].append(workers_num + 1)
+                    k[row[1]].append(row[5])
+                    k[row[1]].append(row[5])
+                    k[row[1]].append(int(row[5]))
+                    k[row[1]].append(0)
+                else:
+                    (k.get(row[1]))[0] += 1
+                    (k.get(row[1]))[3] += int(row[5])
+                    (k.get(row[1]))[4] = round((k.get(row[1]))[3] / (k.get(row[1]))[0])
+                    if int(row[5]) > int((k.get(row[1]))[1]):
+                        (k.get(row[1]))[1] = int(row[5])
+                    if int(row[5]) < int((k.get(row[1]))[2]):
+                        (k.get(row[1]))[2] = int(row[5])
+            count += 1
+
+        for v in k.values():
+            del (v[3])
+
+    with open('final.csv', 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(k.keys())
+        writer.writerows(zip(*k.values()))
+
+
+opt = input('Введите ваш выбор цифрой: ')
+if opt == '1':
+    option1(0, {})
+elif opt == '2':
+    option2(0, 0, {})
+elif opt == '3':
+    option3(0, 0, {})
+else:
+    print('Такого варианта нет:(')
